d9f2926c283c595acfc1eb13a293c400cca3c890
Shantam Vijayputra <vshantam@yahoo.com>
2017-07-04 22:04:15 +0530

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import os,sys
+
+import itertools,threading
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from tkinter import *
+
+import tkFileDialog
+
+from PIL import Image
+
+from PIL import ImageTk
+
+import cv2
+
+import time
+
---
+++ main.py
@@ -0,0 +1,19 @@
+
+if __name__=='__main__':
+
+	
+
+	from applic import img_pro
+	
+	panelA=None
+
+	panelB=None
+
+	obj=img_pro()
+
+	obj.Hello()
+
+	obj.Tkwindow()
+	
+	
+
