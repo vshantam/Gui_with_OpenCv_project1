a4a61aac9f1e0f7fa3ad20c311ab307f489de455
Shantam Vijayputra <vshantam@yahoo.com>
2017-09-28 00:41:38 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,28 +1,25 @@
-
-
 from tkinter import *
-
 failures=[]
 failure_max=3
 passwords=[('root','toor')]
-
-def check_cred():
-	print(username.get(), password.get())
-	if (username.get(), password.get()) in passwords:
-		master.destroy()
-	        print('Logged in')
-	        from applic import *
-	else:	
-		failures.append(1)
-		if sum(failures) >= failure_max:
+class Login(object):
+	def check_cred(self):
+		print(username.get(), password.get())
+		if (username.get(), password.get()) in passwords:
 			master.destroy()
-		else:
-        		master.title('Try again. Attempt %i/%i' % (sum(failures)+1, failure_max))
-def login():
-	check_cred()
-global kills
-def kills():
-	master.destroy()
+		        print('Logged in')
+		        from applic import *
+		else:	
+			failures.append(1)
+			if sum(failures) >= failure_max:
+				master.destroy()
+			else:
+        			master.title('Try again. Attempt %i/%i' % (sum(failures)+1, failure_max))
+	def login(self):
+		self.check_cred()
+	def kills(self):
+		master.destroy()
+objs=Login()
 master = Tk()
 master.geometry('720x480')
 Label(master, text="username").grid(row=0)
@@ -31,6 +28,6 @@ username= Entry(master)
 password = Entry(master)
 username.grid(row=0, column=1)
 password.grid(row=1, column=1)
-Button(master, text='Quit', command=kills).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='login', command=login).grid(row=3, column=1, sticky=W, pady=4)
+Button(master, text='Quit', command=objs.kills).grid(row=3, column=0, sticky=W, pady=4)
+Button(master, text='login', command=objs.login).grid(row=3, column=1, sticky=W, pady=4)
 mainloop( )
