3ac05c14a35f9b529e7ba210247e6ffcaf8ab74f
Shantam Vijayputra <vshantam@yahoo.com>
2017-09-28 00:17:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ class mains(object):
                 from login import *
 	@classmethod
 	def Tkwindow(self):
-		global master
+		global master , frame
 		master=Tk()
 		master.geometry('{}x{}'.format(1080,480))
 		greetings="{}!, PLease select an option Below to Continue".format("shantam")#my name
 		msg=Label(master,text=greetings)
 		msg.configure(background='lightgreen', font=('times', 24, 'italic'))
 		msg.pack()
-		global frame
 		frame = Frame(master)
     		frame.pack()
     		button = Button(frame, text="Agree and Continue.", fg="blue",command=self.Hello)
