d522d617d86190cbe6dd649f6dfe55d30b594fe1
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2017-11-08 01:26:03 +0530

    Fixed import of tkFileDialog
    Fixed import of tkFileDialog based on new Tkinter Update

--- lib.py
+++ lib.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
-import tkFileDialog
+
+try:
+    import tkFileDialog
+except:
+    from tkinter import filedialog as tkFileDialog
 from PIL import Image
 from PIL import ImageTk
 import cv2
-
